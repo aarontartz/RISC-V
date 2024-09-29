681a59edab30131efcc41e93022560c0f6f635aa
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2024-09-29 00:55:29 -0700

    Update extend_tb.v

--- sim/extend_tb.v
+++ sim/extend_tb.v
@@ -4,20 +4,34 @@
 // 
 // Create Date: 09/28/2024 04:52:49 PM
 // Module Name: extend_tb
+// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module extend_tb;
-    reg [11:0] instr;
+    reg sel;
+    reg [31:0] instr;
     wire [31:0] imm_ext;
+    
+    reg clk = 1;
+    
+    localparam [31:0] TEST1 = 32'hFFC4A303;  // lw test
+    localparam [31:0] TEST2 = 32'h0064A423;  // sw test
 
     extend extend_inst (
-        .instr(instr),
+        .instr(instr[31:7]),
+        .sel(sel),
         .imm_ext(imm_ext)
     );
     
+    always #100 clk = ~clk;
+    
     initial begin
-        instr <= 12'hFFC;
+        sel <= 0;
+        instr <= TEST1;
+        @(posedge clk);
+        sel <= 1;
+        instr <= TEST2;
     end
 
 endmodule
