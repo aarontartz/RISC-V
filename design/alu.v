f405d54fbb3212de55678f1029311c2721c7a6a7
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2024-09-29 02:55:39 -0700

    Update alu.v

--- design/alu.v
+++ design/alu.v
@@ -14,24 +14,31 @@ module alu(
     input wire [31:0] op_a,  // operand a
     input wire [31:0] op_b,
     input wire [2:0] sel,
+    output wire is_zero,
     output wire [31:0] result
     );
     
+    reg is_zero_reg;
     reg [31:0] result_reg;
+    assign is_zero = is_zero_reg;
     assign result = result_reg;
     
     always @(*) begin
+        is_zero_reg = 0;                    // default to zero
         case (sel)
             3'b000:
-                result_reg = op_a + op_b;  // addition
-            3'b001:
-                result_reg = op_a - op_b;  // subtraction
+                result_reg = op_a + op_b;   // addition
+            3'b001: begin
+                result_reg = op_a - op_b;   // subtraction
+                if (result_reg == 0)
+                    is_zero_reg = 1;
+            end
             3'b010:
-                result_reg = op_a & op_b;  // logical AND
+                result_reg = op_a & op_b;   // logical AND
             3'b011:
-                result_reg = op_a | op_b;  // logical OR
+                result_reg = op_a | op_b;   // logical OR
             3'b101:
-                result_reg = (op_a < op_b) ? 1 : 0;  // less than comparison
+                result_reg = (op_a < op_b) ? 1 : 0; // less than comparison
             default:
                 result_reg = 32'bxxxx_xxxx_xxxx_xxxx;
         endcase
