b88e00d5078551f25743ad2623d0511123569ad7
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2024-09-29 02:55:10 -0700

    Update extend.v

--- design/extend.v
+++ design/extend.v
@@ -11,10 +11,24 @@
 
 module extend(
     input wire [24:0] instr,
-    input wire sel,             // sw: sel = 1, lw: sel = 0
+    input wire [1:0] sel,       // lw: sel = 00, sw: sel = 01, beq: sel = 10
     output wire [31:0] imm_ext
     );
     
-    assign imm_ext = (sel) ? {{20{instr[24]}}, instr[24:18], instr[4:0]} : {{20{instr[24]}}, instr[24:13]};
+    reg [31:0] imm_ext_reg;
+    assign imm_ext = imm_ext_reg;
+    
+    always @(*) begin
+        case (sel)
+            2'b00:  // imm_ext = sign-extended immediate
+                imm_ext_reg = {{20{instr[24]}}, instr[24:13]};  // I-type (e.g. lw)
+            2'b01:  // imm_ext = sign-extended immediate
+                imm_ext_reg = {{20{instr[24]}}, instr[24:18], instr[4:0]};  // S-type (e.g. sw)
+            2'b10:  // imm_ext = branch offset
+                imm_ext_reg = {{20{instr[24]}}, instr[0], instr[23:18], instr[4:1], 1'b0};  // B-type (e.g. beq)
+            default:
+                imm_ext_reg = 32'bxxxx_xxxx_xxxx_xxxx;
+        endcase
+    end
     
 endmodule
